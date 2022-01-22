861142da685a47cdf6298033a1cde6b44ee8030c
Mayank Kumar <mayank22oct@gmail.com>
2022-01-23 00:52:22 +0530

    feat: added basic proxy

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"github.com/valyala/fasthttp"
+)
 
 func main() {
-
+	fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
+		ctx.Request.SetHost("127.0.0.1:8000")
+		err := fasthttp.Do(&ctx.Request, &ctx.Response)
+		if err != nil {
+			return
+		}
+	})
 }
